650466e95573c3633c00ad313a466eebd0354ecf
Luczak Kamil <kamilluczak@luczakweb.pl>
2018-11-17 11:10:21 +0100

    Add UpTime module

---
+++ tests/ut/modules/time/UpTimeModuleTests.cpp
@@ -0,0 +1,61 @@
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+#include <memory>
+#include <string>
+#include "helpers/mocks/TimeInterfaceMock.hpp"
+#include "modules/UpTime.hpp"
+
+namespace {
+constexpr time_t startTimeT{1517627100};
+}  // namespace
+
+struct UpTimeModuleFixture : public ::testing::Test {
+    std::unique_ptr<modules::UpTime> upTimeModule;
+    helpers::mocks::TimeInterfaceMock timeInterfaceMock;
+    const interfaces::TimePoint startTime;
+
+   protected:
+    UpTimeModuleFixture()
+        : startTime{std::chrono::system_clock::from_time_t(startTimeT)} {
+        EXPECT_CALL(timeInterfaceMock, actualTime())
+            .WillOnce(::testing::Return(startTime));
+
+        upTimeModule = std::make_unique<modules::UpTime>(timeInterfaceMock);
+    }
+
+    void expectOutput(const std::string& expectedOutput) {
+        const auto& output = upTimeModule->printModule();
+        EXPECT_EQ(output, expectedOutput);
+    }
+};
+
+TEST_F(UpTimeModuleFixture, testOutputFormat) {
+    EXPECT_CALL(timeInterfaceMock, actualTime())
+        .WillOnce(::testing::Return(startTime));
+
+    expectOutput({"Up 00:00:00"});
+}
+
+TEST_F(UpTimeModuleFixture, testSecondsOutput) {
+    interfaces::TimePoint timePoint{startTime + std::chrono::seconds{5}};
+    EXPECT_CALL(timeInterfaceMock, actualTime())
+        .WillOnce(::testing::Return(timePoint));
+
+    expectOutput({"Up 00:00:05"});
+}
+
+TEST_F(UpTimeModuleFixture, testMinutsOutput) {
+    interfaces::TimePoint timePoint{startTime + std::chrono::minutes{54}};
+    EXPECT_CALL(timeInterfaceMock, actualTime())
+        .WillOnce(::testing::Return(timePoint));
+
+    expectOutput({"Up 00:54:00"});
+}
+
+TEST_F(UpTimeModuleFixture, testHoursOutput) {
+    interfaces::TimePoint timePoint{startTime + std::chrono::hours{21}};
+    EXPECT_CALL(timeInterfaceMock, actualTime())
+        .WillOnce(::testing::Return(timePoint));
+
+    expectOutput({"Up 21:00:00"});
+}
