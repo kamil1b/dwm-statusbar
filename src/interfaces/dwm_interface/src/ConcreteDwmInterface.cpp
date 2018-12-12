395c63b95aaa3deb0fa70ee44336e103f76ca370
Kamil Łuczak <github@luczakweb.pl>
2018-12-12 19:53:05 +0100

    Change dwm interface (#10)

--- src/interfaces/dwm_interface/src/ConcreteDwmInterface.cpp
+++ src/interfaces/dwm_interface/src/ConcreteDwmInterface.cpp
@@ -1,16 +1,30 @@
 #include "interfaces/dwm_interface/ConcreteDwmInterface.hpp"
 #include <stdexcept>
-#include <stdlib.h>
-#include <sys/wait.h>
 
-namespace {
-constexpr auto command = "xsetroot -name '";
+#ifdef __GNUC__
+#pragma GCC diagnostic ignored "-Wold-style-cast"
+#endif // GNUC
+
+interfaces::ConcreteDwmInterface::ConcreteDwmInterface()
+{
+    Display* dpy = XOpenDisplay(nullptr);
+    if (dpy == nullptr) {
+        throw std::runtime_error { "XOpenDisplay: Failed to open display" };
+    }
+    display = std::make_unique<Display>(*dpy);
+}
+
+interfaces::ConcreteDwmInterface::~ConcreteDwmInterface()
+{
+    XStoreName(display.get(), DefaultRootWindow(display.get()), nullptr);
+    XCloseDisplay(display.get());
 }
 
 void interfaces::ConcreteDwmInterface::printStatusBar(const std::string& statusBar)
 {
-    const auto exec_command = command + statusBar + "'";
-    const auto result = system(exec_command.c_str());
-    if (not WIFEXITED(result))
-        throw std::runtime_error { "xsetroot command not work correctly!" };
+    if (XStoreName(display.get(), DefaultRootWindow(display.get()), statusBar.c_str())
+        < 0) {
+        throw std::runtime_error { "XStoreName: Allocation failed" };
+    }
+    XFlush(display.get());
 }
