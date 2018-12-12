e0ea1622829a9470eb6949a0282f87e906c605b7
Luczak Kamil <kamilluczak@luczakweb.pl>
2018-12-12 19:14:28 +0100

    Change dwm interface

--- src/interfaces/dwm_interface/include/interfaces/dwm_interface/ConcreteDwmInterface.hpp
+++ src/interfaces/dwm_interface/include/interfaces/dwm_interface/ConcreteDwmInterface.hpp
@@ -1,9 +1,16 @@
 #pragma once
 #include "interfaces/dwm_interface/DwmInterface.hpp"
+#include <X11/Xlibint.h>
+#include <memory>
 #include <string>
 
 namespace interfaces {
 struct ConcreteDwmInterface final : public interfaces::DwmInterface {
+    ConcreteDwmInterface();
+    ~ConcreteDwmInterface();
     void printStatusBar(const std::string& statusBar) override;
+
+private:
+    std::unique_ptr<Display> display;
 };
 } // namespace interfaces
